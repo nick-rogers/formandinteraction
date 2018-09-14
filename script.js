a6d3ea7259e29e874785fda2563e0183c4ffc601
nick-rogers <31479865+nick-rogers@users.noreply.github.com>
2018-09-14 17:37:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,82 +1,38 @@
 $(document).ready(function(){
 
 
-var topScroll = $(window).scrollTop();
-
-console.log('topScroll: ' + topScroll);
-
-
-/*var activeOne = true,
-	activeTwo = false,
-	activeThree = false;*/
-
-/*$(window).scroll(function() {
-   var hT = $('#bd01').offset().top,
-       hH = $('#bd01').outerHeight(),
-       wH = $(window).height(),
-       wS = $(this).scrollTop();
-   if (wS > ((hT+hH)-125)){
-       console.log('H1 on the view!');
-   }
-});*/
-
-
-/*
-$(window).scroll(function(){
-	if(activeOne){
-		scrollCheck('#bd01');
-	}else if(activeTwo){
-		scrollCheck('#bd02');
-	}
-});
-
-
-
-function scrollCheck(idName){
-	var hT = $(idName).offset().top,
-       hH = $(idName).outerHeight(),
-       wH = $(window).height(),
-       wS = $(this).scrollTop();
-   	if (wS > ((hT+hH)-125)){
-       var theId = $(idName).attr('id');
-       console.log('theId: ' + theId);
-       if(theId == '#bd01'){
-
-       }
-       else if(theId == '#bd02'){
-
-       }
-
-   }
-}*/
 var bd1Top = ($('#bd01').offset().top) + ($('#bd01').outerHeight()) - 125;
 var bd2Top = ($('#bd02').offset().top) + ($('#bd02').outerHeight()) - 125;
 
 var topDis = [bd1Top, bd2Top];
 
+var topScroll = $(window).scrollTop();
+
+if(topScroll < topDis [0]){
+		$('.change-week').text('1 (Aug 27 - 31)');
+	}
+	else if(topScroll < topDis[1]){
+		$('.change-week').text('2 (Sep 3 - 7)');
+	}
+	else if(topScroll > topDis[1]){
+		$('.change-week').text('3 (Sep 10 - 14)');
+	}
+
+console.log('topScroll: ' + topScroll);
 
 $(window).scroll(function(){
-	if ($(window).scrollTop() > topDis[0]){
-       var compare = $(window).scrollTop();
-       $(window).scroll(function(){
-       		var newTop = $(window).scrollTop();
-       		console.log('old top: ' + compare + ' new top: ' + newTop);
-       });
-   	}
-	/*var hT = $('#bd01').offset().top,
-       hH = $('#bd01').outerHeight(),
-       wH = $(window).height(),
-       wS = $(this).scrollTop();
-   if (wS > ((hT+hH)-125)){
-       console.log('H1 on the view!');
-   }*/
+	var wS = $(this).scrollTop();
 
+	if(wS < topDis [0]){
+		$('.change-week').text('1 (Aug 27 - 31)');
+	}
+	else if(wS < topDis[1]){
+		$('.change-week').text('2 (Sep 3 - 7)');
+	}
+	else if(wS > topDis[1]){
+		$('.change-week').text('3 (Sep 10 - 14)');
+	}
 });
-
-
-
-
-
 
 
 
